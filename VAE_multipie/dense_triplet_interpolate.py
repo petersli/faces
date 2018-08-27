e5f04560a8568aff896fcb3ee5755cccb53fba5f
Peter Li <peter.s.li93@gmail.com>
2018-08-26 20:48:46 -0700

    remove get_latent_vecs

--- VAE_multipie/dense_triplet_interpolate.py
+++ VAE_multipie/dense_triplet_interpolate.py
@@ -259,8 +259,8 @@ def test(epoch):
 		dp0_img, dp9_img, dp1_img = setAsVariable(dp0_img, dp9_img, dp1_img )
 
 
-		z_dp9, z_per_dp9, z_exp_dp9 = model.get_latent_vectors(dp9_img)
-		z_dp1, z_per_dp1, z_exp_dp1 = model.get_latent_vectors(dp1_img)
+		recon_dp9, z_dp9, z_per_dp9, z_exp_dp9 = model(dp9_img)
+		recon_dp1, z_dp1, z_per_dp1, z_exp_dp1 = model(dp1_img)
 
 		optimizer.zero_grad()
 		model.zero_grad()
