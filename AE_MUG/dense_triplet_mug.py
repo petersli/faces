67db4c77f6c2ca41f8f633bc4400c5576b8f1b66
Peter Li <peter.s.li93@gmail.com>
2018-09-03 17:29:39 -0700

    batchsize = 25

--- AE_MUG/dense_triplet_mug.py
+++ AE_MUG/dense_triplet_mug.py
@@ -31,7 +31,7 @@ import WaspNet_dany as WaspNet
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=8)
-parser.add_argument('--batchSize', type=int, default=100, help='input batch size')
+parser.add_argument('--batchSize', type=int, default=25, help='input batch size')
 parser.add_argument('--imageSize', type=int, default=64, help='the height / width of the input image to network')
 parser.add_argument('--ngf', type=int, default=64)
 parser.add_argument('--ndf', type=int, default=64)
